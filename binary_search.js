e6670769e0ab9ef147cbd5bf365f8f08fdb4ca4d
liyabin <2750127294@qq.com>
2018-08-16 09:32:00 +0800

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,25 @@
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+  var guess;
+    
+    while (max >= min) {
+        guess = Math.floor((max + min) / 2);
+        if (array[guess] === targetValue) {
+            return guess;
+        }
+        if (array[guess] < targetValue) {
+            min = guess + 1;
+        } 
+        
+        else {
+            max = guess - 1;
+        }
+    }
+	return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
